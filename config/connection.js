fa72f53aa02f23d1882fb8357376c6c53a5a3e01
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-05-02 18:12:54 -0500

    small fix

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSURL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-  host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "k7x95b6vm8y52f6t",
-  password: "wnucp5fp8g1cc02c",
-  database: "	viaqibl8u9vfv3cs"
+  host: "localhost",
+  user: "root",
+  password: "2425",
+  database: "burgers_DB"
 });
 };
 
