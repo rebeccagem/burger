fb9f037b877604dea2d2cb2dea1b68a157c91847
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-05-02 21:19:42 -0500

    trying to adjust table values in JAWS

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
 -- Insert a set of records.
-INSERT INTO burgers (name) VALUES ('Cheese Burger');
-INSERT INTO burgers (name) VALUES ('Veggie Burger');
-INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey', true);
+INSERT INTO burgers (name, devoured) VALUES ('Rabbit', true);
